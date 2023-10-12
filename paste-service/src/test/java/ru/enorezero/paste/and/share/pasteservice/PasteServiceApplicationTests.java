368475bf3f7d10f2d1ec27d4ce83512753313939
Enorezero <98647618+Enorezero@users.noreply.github.com>
2023-10-12 20:42:33 +1000

    add modules

---
+++ paste-service/src/test/java/ru/enorezero/paste/and/share/pasteservice/PasteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.enorezero.paste.and.share.pasteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
